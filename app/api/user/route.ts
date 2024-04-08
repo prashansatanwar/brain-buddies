48de7411ac8bd31d0a0fcc3f202224b157e18f37
prashansatanwar <prashansatanwar@gmail.com>
2024-04-08 18:08:56 +0800

    Updated user api

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,8 +1,7 @@
-import { NextApiRequest, NextApiResponse } from 'next';
 import { prisma } from '../../prismaClient'
 import { NextResponse } from 'next/server';
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET() {
     const users = await prisma.user.findMany();
     return NextResponse.json({"users":users});
 }
